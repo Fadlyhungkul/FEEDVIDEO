e177e97664f38b86621877e8f769dc1d063f819f
Rizalsputr <zaltsaputra28@gmail.com>
2024-02-19 09:59:57 +0700

    first commit

---
+++ app/Models/Video.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class video extends Model
+{
+    use HasFactory;
+
+    protected $table = 'videos';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'video', 'caption'
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+
